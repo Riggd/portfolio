065388194653cbd1ba69fcc9786d02dab237c8fc
Derek Onay <derek.onay@gmail.com>
2025-01-24 21:03:12 -0500

    Removed old code in themeswitcher.js

--- src/js/themeswitcher.js
+++ src/js/themeswitcher.js
@@ -1,57 +1,5 @@
-/* // function to set a given theme/color-scheme
- function setTheme(themeName) {
-    localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
-}
-
-// function to toggle between light and dark theme
-function toggleTheme() {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-light');
-    } else {
-        setTheme('theme-dark');
-    }
-}
-
-// Immediately invoked function to set the theme on initial load
-(function () {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-dark');
-        document.getElementById('slider').checked = false;
-    } else {
-        setTheme('theme-light');
-      document.getElementById('slider').checked = true;
-    }
-})();*/
-
-
-/* document.addEventListener("DOMContentLoaded", (event) => {
-    const toggleButton = document.getElementById("theme-toggle");
-
-    //is this necessary
-    if (localStorage.getItem("theme") === "theme-dark") {
-    }
-
-    toggleButton.addEventListener("click", () => {
-        if (
-            document.documentElement.classList.contains(
-                "theme-dark"
-            )
-        ) {
-            document.documentElement.classList.remove("theme-dark");
-            //Might have a more elegant solution to work out here.
-            document.documentElement.classList.add("theme-light");
-            localStorage.setItem("theme", "light");
-        } else {
-            document.documentElement.classList.add("theme-dark");
-            localStorage.setItem("theme", "dark");
-        }
-    });
-}); */
-
 var toggle = document.getElementById("theme-toggle");
 
-//var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
 var storedTheme = sessionStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
 if (storedTheme) {
     document.documentElement.setAttribute('data-theme', storedTheme);
@@ -66,6 +14,5 @@ toggle.onclick = function() {
     }
 
     document.documentElement.setAttribute('data-theme', targetTheme);
-    //localStorage.setItem('theme', targetTheme);
     sessionStorage.setItem('theme', targetTheme);
 };
