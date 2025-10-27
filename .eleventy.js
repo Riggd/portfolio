991ca52cef82d2bf70b1d51773e1907fbe0d7b21
Derek Onay <derek.onay@gmail.com>
2025-10-26 23:50:33 -0400

    Added 404 page

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ import markdownItAttrs from 'markdown-it-attrs';
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import eleventySass from "eleventy-sass";
 import esbuild from 'esbuild';
+import fs from "fs";
 
 export default function(eleventyConfig) {
 
@@ -76,6 +77,22 @@ export default function(eleventyConfig) {
         },
     });
 
+    // 404 page for local development
+    eleventyConfig.setBrowserSyncConfig({
+		callbacks: {
+			ready: function(err, bs) {
+
+				bs.addMiddleware("*", (req, res) => {
+					const content_404 = fs.readFileSync('public/404.html');
+					// Add 404 http status code in request header.
+					res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+					// Provides the 404 content without redirect.
+					res.write(content_404);
+					res.end();
+				});
+			}
+		}
+	});
     
 
 
